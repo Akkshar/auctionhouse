9903b829af00e23d39bc44a507d3cf83b4e4a702
Akkshar <57217402+Akkshar@users.noreply.github.com>
2021-05-22 21:41:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+input = input("")
+
+if(input == '!game_shop'):
+    print("Rings - can be sold and bought but cannot be auctioned\n")
+    print("common_ring = 15000\nProvides you 2% happiness, pretty cheap option.\n")
+    print("uncommon_ring = 20000\nProvides you 3% happiness, little bit of value for little bit of money.\n")
+    print("rare_ring = 30000\nProvides you 5% happiness, gives your pet happiness as a bonus!\n")
+    print("epic_ring = 55000\nProvides 10% happiness, liitle bit of profit\n")
+    print("legendary_ring = 100000\nProvides 20% happiness, only few players have this.\n")
+    print("mythic_ring = 150000\nProvide 30% happiness, great profit great price! Only top ten% of players own this.\n")
+
+    print("Traps - can be used but not sold\n ")
+    print("alarm_trap = 100000\nThis Trap stops the person who is trying to steal from you and may cost them a fine\n")
+    print("invisible_trap = 200000\n This trap is like an alarm trap but it works on invisible people.\n")
+    print("slowness_trap = 150000\n This trap catches the robber and applies slowness affect on him which will get him caught with epople who dont have a trap\n")
+    print("freeze_trap = 275000\n This trap applies the freeze effect on the robber which prevents him from stealing from anyone.\n")
+
+    print("collectibles - can be sold and auctioned\n")
+    print("apartment = 100000\n This adds 10% safety\n")
+    print("row_house = 300000\n This adds 25% safety\n")
+    print("condo = 500000\n This adds 35% safety\n")
+    print("skyscraper = 1000000\n This adds 50% safety")
+
+    print("vehicles - can be used to get to different places\n")
+    print("ferrari = 300000\n Get your ferrari and yeehaw to places!\n")
+    print("lamborghini = 300000\n Competitor of Ferrari.\n")
+    print("buggati = 300000\n I am speeeeeeeeeeeed.\n")
+    print("helicopter = 750000\n Nice Aerial view btw.\n")
+    print("private_jet = 1500000\n Faster than the helicopter.\n")
+
+    print("Devices - can be used and sold")
+    print("xp_cookie = 10000 \n Use this to get an exp multiplier\n")
+    print("energy_boost = 25000 \n Use this to get your tasks done faster\n")
+
+
---
+++ welcome.py
@@ -0,0 +1,9 @@
+print("welcome to auction house! This is a minigame with a currency\nand you have to auction your items to gain money for exp multiplier and other items!")
+answer = input("are you ready for the challenges! type yes or no ")
+
+if(answer == 'yes'):
+    print("ok, my name is auction_house. My prefix is ~game and _ for spaces\n")
+    print("Ex: ~game_shop")
+
+else:
+    print("why are you even here?")
